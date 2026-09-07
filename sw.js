18553a846fa684cbd4e29ff6a78c97a0c3cc67c0
sossiomormile <sossiomormile@libero.it>
2026-09-07 12:07:27 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-const C="condomini-v2-20260907";self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json"])))});self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const C="condomini-v2-1-20260907";
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json"])))});
+self.addEventListener("activate",e=>e.waitUntil(Promise.all([
+  caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))),
+  self.clients.claim()
+])));
+self.addEventListener("fetch",e=>{
+  e.respondWith(fetch(e.request).then(r=>{
+    const copy=r.clone();
+    caches.open(C).then(c=>c.put(e.request,copy)).catch(()=>{});
+    return r;
+  }).catch(()=>caches.match(e.request)));
+});
