a9652ea99cfb0048c1c71471eebdbfec469b30c5
sossiomormile <sossiomormile@libero.it>
2026-09-07 12:22:00 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
-const C="condomini-v2-2-20260907";
-self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json"])))});
-self.addEventListener("activate",e=>e.waitUntil(Promise.all([
-  caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))),
-  self.clients.claim()
-])));
-self.addEventListener("fetch",e=>{
-  if(e.request.mode==="navigate"){e.respondWith(fetch(e.request).catch(()=>caches.match("./index.html")));return;}
-  e.respondWith(fetch(e.request).then(r=>{let cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp)).catch(()=>{});return r;}).catch(()=>caches.match(e.request)));
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',event=>{
+  event.waitUntil((async()=>{
+    await self.registration.unregister();
+    const cs=await self.clients.matchAll();
+    for(const c of cs) c.navigate(c.url);
+  })());
 });
