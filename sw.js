0222645a353313694d473cb82654b0848301fc97
sossiomormile <sossiomormile@libero.it>
2026-09-07 15:30:31 +0200

    Aggiorna service worker per V2.5

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>{
   event.waitUntil((async()=>{
+    const keys=await caches.keys();
+    await Promise.all(keys.map(k=>caches.delete(k)));
     await self.registration.unregister();
-    const cs=await self.clients.matchAll();
-    for(const c of cs) c.navigate(c.url);
+    const clients=await self.clients.matchAll({type:'window'});
+    for(const c of clients) c.navigate(c.url);
   })());
 });
