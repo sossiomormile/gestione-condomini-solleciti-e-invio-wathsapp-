86e05fb3e7a8876037dfe784de47eabccba39af4
sossiomormile <sossiomormile@libero.it>
2026-09-09 21:24:43 +0200

    Force one-time reprocessing after row alignment engine update

---
+++ engine-refresh-v1.js
@@ -0,0 +1,9 @@
+(()=>{
+'use strict';
+const MARK='condo_engine_refresh_rowfix2_done';
+if(localStorage.getItem(MARK)==='1')return;
+try{
+  localStorage.removeItem('condo_drive_sync_state_v7');
+  localStorage.setItem(MARK,'1');
+}catch(e){}
+})();
